f797261878897a6df2c5d5c58e30f65aa3eddb8e
Arun Kumar Surya Prakash <arsurya@microsoft.com>
2016-11-07 10:48:42 +0530

    samplee

--- ContinuousIntegration/ContinuousIntegration/App.cs
+++ ContinuousIntegration/ContinuousIntegration/App.cs
@@ -11,33 +11,10 @@ namespace ContinuousIntegration
     {
         public App()
         {
-            // testing // // //
+            // testing
             //The root page of your application
             MainPage = new ContentPage
             {
-             protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-         protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }   VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
                 Content = new StackLayout
                 {
                     VerticalOptions = LayoutOptions.Center,
@@ -51,11 +28,7 @@ namespace ContinuousIntegration
             };
         }
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
+       
         protected override void OnSleep()
         {
             // Handle when your app sleeps
@@ -65,5 +38,10 @@ namespace ContinuousIntegration
         {
             // Handle when your app resumes
         }
+         protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
     }
 }
