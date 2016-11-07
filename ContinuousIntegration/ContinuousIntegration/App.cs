27f06e6cf490fed67623f2f3e827f7590b30b78d
Arun Surya Prakash <arun.nov04@gmail.com>
2016-11-07 10:39:21 +0530

    Update App.cs

--- ContinuousIntegration/ContinuousIntegration/App.cs
+++ ContinuousIntegration/ContinuousIntegration/App.cs
@@ -15,6 +15,29 @@ namespace ContinuousIntegration
             //The root page of your application
             MainPage = new ContentPage
             {
+             protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+         protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }   VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
                 Content = new StackLayout
                 {
                     VerticalOptions = LayoutOptions.Center,
